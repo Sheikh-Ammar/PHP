6f35a1dbf96c45bad745f2f5967283348ca4d875
Sheikh Ammar <sheikhammar568@gmail.com>
2022-10-22 15:54:08 +0500

    Add Portfolio And Rest-Api Work Complete

---
+++ Portfolio/recent.php
@@ -0,0 +1,17 @@
+<?php include "header.php"; ?>
+
+
+
+
+
+    <section>
+        <div class="container my-5">
+            <img src="./images/recent.png" alt="Recent Under Construction" width="100%" height="100%">
+        </div>
+    </section>
+    <div class="text-center my-5">
+        <a href="index.php" class="btn btn-success my-3">Back To Home</a>
+    </div>
+    <br><br><br>
+
+<?php include "footer.php"; ?>
